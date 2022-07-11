53632eae411b2d2cdc844344f62a26fe7b106159
Dima <uvarov3123@gmail.com>
2022-07-11 18:24:05 +0300

    add style sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 @@include("./lib/jquery.fancybox.min.js")
 @@include("./lib/wpcf7.js")
+@@include("./lib/select.js")
 /*end of inludes*/
-	
 
-$(document).ready(function(){
-	
-	$(window).on("wl_resize",function(event,ww){
+
+$(document).ready(function () {
+
+	$(window).on("wl_resize", function (event, ww) {
 		console.log("some_code");
 	});
 
-}); 
+});
